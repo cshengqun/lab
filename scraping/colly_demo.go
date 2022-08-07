217be42d0e21c17d5e1c07f7dfe4eb69f0dfef7e
csq <caishengqun@bytedance.com>
2022-08-07 11:57:14 +0800

    feat: test goquery

--- scraping/colly_demo.go
+++ scraping/colly_demo.go
@@ -3,16 +3,37 @@ package scraping
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"strings"
 )
+
 
 func CollyScraping() {
 	c := colly.NewCollector()
-	c.OnHTML("a[href]", func(e *colly.HTMLElement){
-		e.Request.Visit(e.Attr("href"))
+	c.OnHTML(".info", func(e *colly.HTMLElement){
+		// e.Request.Visit(e.Attr("href"))
+		test := e.ChildText(".rating_num")
+		fmt.Printf("test:%s\n", test)
+		titla := e.ChildText(".hd a span:first-child")
+		fmt.Printf("titla:%s\n", titla)
+		return
+		name := e.DOM.Find(".hd a").Text()
+		rate := e.DOM.Find(".rating_num").Text()
+		texts := strings.Split(name, "/")
+		if len(texts) > 0 {
+			fmt.Printf("%s %s\n", strings.TrimSpace(texts[0]), rate)
+		}
+
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("visiting", r.URL)
 	})
-	c.Visit("http://go-colly.org/")
+	c.OnResponse(func(response *colly.Response) {
+		fmt.Println("response:", string(response.Body))
+	})
+	for start:=0; start<=250; start= start + 25 {
+		url := fmt.Sprintf("https://movie.douban.com/top250?start=%d&filter=", start)
+		c.Visit(url)
+	}
+
 }
